cc1ec4615b2be796ac8ac9e16ddb54916a9ab458
Laeqoun <Dmanuylov@gmail.com>
2016-11-29 20:28:05 +0300

    не большая правка размера формы и добавление в гит игнор

--- HWMB/MainForm.Designer.cs
+++ HWMB/MainForm.Designer.cs
@@ -35,7 +35,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(684, 361);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "Бот Heroeswm";
